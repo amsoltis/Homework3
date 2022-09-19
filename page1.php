9148cd89ba06854a2cb529b21995acb31a1627be
Andrew Soltis <ams.soltis@ou.edu>
2022-09-19 14:03:15 -0500

    tEST

--- page1.php
+++ page1.php
@@ -1,16 +1,6 @@
 <?php require_once("header.php"); ?>
 <!doctype html>
 <html lang="en">
-  <head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>Bootstrap demo</title>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-iYQeCzEYFbKjA/T2uDLTpkwGzCiq6soy8tYaI1GyVh/UjpbCx/TYkiZhlZB6+fzT" crossorigin="anonymous">
-  </head>
-  <body>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-u1OknCvxWvY5kfmNBILK2hRnQC3Pr17a+RTT6rIHI7NnikvbZlHgTPOOmMi466C8" crossorigin="anonymous"></script>
-  </body>
-</html>
 
 <?php
 $servername = "localhost";
@@ -33,20 +23,19 @@ $result = $conn->query($sql);
     while($row = $result->fetch_assoc()) {
 
 
-        {
-            ?>
-            <tr>
-                <!-- FETCHING DATA FROM EACH
-                    ROW OF EVERY COLUMN -->
-                <td><?php echo $rows['InstructorID'];?></td>
-                <td><?php echo $rows['LastName'];?></td>
-                <td><?php echo $rows['FirstName'];?></td>
-            </tr>
-            <?php
-                }
-            ?>
-        </table>
-    </section>
-</body>
- 
-</html>
+        echo <tr>
+                  <td> <?$row["InstructorID"]?></td>
+                  <td> <?$row["LastName"]?></td>
+                  <td> <?$row["FirstName"]?></td>
+                </tr>;
+    }
+} else {
+    echo "0 results";
+} 
+?>
+       </tbody>
+    </div>
+</table>
+}
+$conn->close();
+?>
