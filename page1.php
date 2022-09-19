15a75687bc201e2e98227687c8301a0f4654b920
Andrew Soltis <ams.soltis@ou.edu>
2022-09-19 13:56:04 -0500

    Test

--- page1.php
+++ page1.php
@@ -33,11 +33,66 @@ $result = $conn->query($sql);
     while($row = $result->fetch_assoc()) {
 
 
-        echo '<tr>
-                  <td scope="row">' . $row["InstructorID"]. '</td>
-                  <td>' . $row["LastName"] .'</td>
-                  <td> '.$row["FirstName"] .'</td>
-                </tr>';
+        echo '
+2
+3
+4
+5
+6
+7
+8
+9
+10
+11
+12
+13
+14
+15
+16
+17
+18
+19
+20
+21
+22
+23
+24
+25
+26
+27
+28
+29
+30
+<div class="container">
+ <div class="col-md-12">
+ <div class="page-header">
+ <h1>
+ How to use bootstrap tables to  Display data from MySQL using PHP
+ </h1>
+ </div>
+ 
+ 
+ <div class="panel panel-success">
+ <div class="panel-heading "> 
+ <span class=""> This Source Code Provided By<br>
+ <a href="http://sourcecodessite.com">SourceCodeSite.com</a> </span> 
+ 
+ </div>
+ 
+ <div class="panel-body">
+ <div class="row">
+ <div class="col-md-12">
+ 
+ <table id="table"
+                 	data-show-columns="true"
+                 data-height="460">
+ </table>
+ </div>
+ </div>
+</div> 
+</div>
+</div>
+</div>';
 
 
 
