cacfb488ecdbab34bd2e7ba61107ae6079ab8f2f
Andrew Soltis <ams.soltis@ou.edu>
2022-09-21 13:47:36 -0500

    test

--- page1.php
+++ page1.php
@@ -21,8 +21,6 @@ $result = $conn->query($sql);
  if ($result->num_rows > 0) {
     // output data of each row
     while($row = $result->fetch_assoc()) {
-
-
         ?>
   <tr>
     <td><?=$row["InstructorID"]?></td>
