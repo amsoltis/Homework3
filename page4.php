c611f207a99bff7bef95db51bba3d544c7c00636
Andrew Soltis <ams.soltis@ou.edu>
2022-09-21 14:26:08 -0500

    Age

--- page4.php
+++ page4.php
@@ -1,8 +1,26 @@
-<?php require_once("header.php"); ?>
 <!doctype html>
+<?php require_once("header.php"); ?>
 <html lang="en">
-
-<?php
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Cars</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-gH2yIJqKdNHPEq0n4Mqa/HGKIhSkIHeL5AyhkYV8i59U5AR6csBvApHHNl/vI1Bx" crossorigin="anonymous">
+  </head>
+  <body>
+    <h1>Cars</h1>
+<table class="table table-striped">
+  <thead>
+    <tr>
+      <th>Bird ID</th>
+      <th>Name</th>
+      <th>Color</th>
+      <th>Age</th>
+      <th></th>
+    </tr>
+  </thead>
+  <tbody>
+    <?php
 $servername = "localhost";
 $username = "asoltiso_asoltis";
 $password = "OUcreate!";
@@ -13,34 +31,36 @@ $conn = new mysqli($servername, $username, $password, $dbname);
 // Check connection
 if ($conn->connect_error) {
   die("Connection failed: " . $conn->connect_error);
-} 
+}
 
-$sql = "SELECT * FROM Birds";
+$sql = "SELECT * from Birds";
 $result = $conn->query($sql);
-       
- if ($result->num_rows > 0) {
-    // output data of each row
-    while($row = $result->fetch_assoc()) {
 
-
-        echo '<tr>
-                  <td scope="row">' . $row["Birdid"]. '</td>
-                  <td>' . $row["Name"] .'</td>
-                  <td> '.$row["Color"] .'</td>
-                  <td> '.$row["Age"] .'</td>
-                  <br>
-                  </tr>';
-    }
-} else {
-    echo "0 results";
-} 
+if ($result->num_rows > 0) {
+  // output data of each row
+  while($row = $result->fetch_assoc()) {
 ?>
-       </tbody>
-    </div>
-</table>
-<body>
-    <form action="page4post.php" method="post">
-Sort by Color: <input type="text" name="color"><br>
-<input type="submit">
-</form>
+  <tr>
+    <td><?=$row["Birdid"]?></td>
+    <td><?=$row["Name"]?></td>
+    <td><?=$row["Color"]?></td>
+    <td><?=$row["Age"]?></td>
+    <td>
+      <form method="post" action="page5post.php">
+        <input type="hidden" name="id" value="<?=$row["Age"]?>" />
+        <input type="submit" value="Same Age" />
+      </form>
+    </td>
+  </tr>
+<?php
+  }
+} else {
+  echo "0 results";
+}
+$conn->close();
+?>
+  </tbody>
+    </table>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-A3rJD856KowSb7dwlZdYEkO39Gagi7vIsF0jrRAoQmDKKtQBHUuLZ9AsSv4jD4Xa" crossorigin="anonymous"></script>
   </body>
+</html>
