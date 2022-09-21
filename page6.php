6bc079b29895a012691d01a33c6619cac98f3a85
Andrew Soltis <ams.soltis@ou.edu>
2022-09-21 16:18:40 -0500

    noe

--- page6.php
+++ page6.php
@@ -22,7 +22,7 @@ if ($conn->connect_error) {
   die("Connection failed: " . $conn->connect_error);
 }
 
-$sql = "SELECT InstructorID, FROM Instructor";
+$sql = "SELECT InstructorID, FirstName, LastName FROM Instructor";
 $result = $conn->query($sql);
 
 if ($result->num_rows > 0) {
