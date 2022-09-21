11568fbe816352cbcbe52592965c53634222291c
Andrew Soltis <ams.soltis@ou.edu>
2022-09-21 15:44:22 -0500

    Instructors

--- page1.php
+++ page1.php
@@ -1,8 +1,24 @@
-<?php require_once("header.php"); ?>
 <!doctype html>
+<?php require_once("header.php"); ?>
 <html lang="en">
-
-<?php
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Instructors</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-gH2yIJqKdNHPEq0n4Mqa/HGKIhSkIHeL5AyhkYV8i59U5AR6csBvApHHNl/vI1Bx" crossorigin="anonymous">
+  </head>
+  <body>
+    <h1>Instructors</h1>
+<table class="table table-striped">
+  <thead>
+    <tr>
+      <th>InstructorID</th>
+      <th>Name</th>
+      <th></th>
+    </tr>
+  </thead>
+  <tbody>
+    <?php
 $servername = "localhost";
 $username = "asoltiso_asoltis";
 $password = "OUcreate!";
@@ -13,25 +29,29 @@ $conn = new mysqli($servername, $username, $password, $dbname);
 // Check connection
 if ($conn->connect_error) {
   die("Connection failed: " . $conn->connect_error);
-} 
+}
 
 $sql = "SELECT InstructorID, LastName, FirstName FROM Instructor";
+//echo $sql;
 $result = $conn->query($sql);
-       
- if ($result->num_rows > 0) {
-    // output data of each row
-    while($row = $result->fetch_assoc()) {
-        ?>
+
+if ($result->num_rows > 0) {
+  // output data of each row
+  while($row = $result->fetch_assoc()) {
+?>
   <tr>
     <td><?=$row["InstructorID"]?></td>
-    <td><a href="page3.php?id=<?=$row["InstructorID"]?>"><?=$row["FirstName"]?></a></td>
+    <td><a href="page3.php?id=<?=$row["InstructorID"]?>"><?=$row["FirstName"] ["LastName"]?></a></td>
   </tr>
 <?php
-    }
+  }
 } else {
-    echo "0 results";
-} 
+  echo "0 results";
+}
+$conn->close();
 ?>
-       </tbody>
-    </div>
-</table>
+  </tbody>
+    </table>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-A3rJD856KowSb7dwlZdYEkO39Gagi7vIsF0jrRAoQmDKKtQBHUuLZ9AsSv4jD4Xa" crossorigin="anonymous"></script>
+  </body>
+</html>
