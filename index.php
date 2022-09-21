ac7db48496fe0c7c5a8bc8728342d25c436b5f1a
Andrew Soltis <ams.soltis@ou.edu>
2022-09-21 16:27:27 -0500

    formatiing

--- index.php
+++ index.php
@@ -7,10 +7,10 @@
   </head>
   <body>
       <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-A3rJD856KowSb7dwlZdYEkO39Gagi7vIsF0jrRAoQmDKKtQBHUuLZ9AsSv4jD4Xa" crossorigin="anonymous"></script>
-      Instructor - Table that shows instructors, and allows you to filter by 'Name'. Filters to only one result as its a appears once in Table.
-Courses - Table that shows courses taught, and allows you to filter by 'Instructor ID' to see only courses taught by that professor.
-Birds - Table shows information about birds, no filter.
-Cars - Table shows information about cars, allows you to filter by car color.
+      <br>Instructor - Table that shows instructors, and allows you to filter by 'Name'. Filters to only one result as its a appears once in Table.</br>
+      <br>Courses - Table that shows courses taught, and allows you to filter by 'Instructor ID' to see only courses taught by that professor.</br>
+      <br>Birds - Table shows information about birds, no filter.</br>
+      <br>Cars - Table shows information about cars, allows you to filter by car color.</br>
   </body>
 </html>
 <?php require_once("footer.php"); ?>
