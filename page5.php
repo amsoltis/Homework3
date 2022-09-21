746220b616f6c989906040231796225970898bd8
Andrew Soltis <ams.soltis@ou.edu>
2022-09-21 14:09:42 -0500

    test

--- page5.php
+++ page5.php
@@ -1,8 +1,25 @@
-<?php require_once("header.php"); ?>
 <!doctype html>
 <html lang="en">
-
-<?php
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Courses</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-gH2yIJqKdNHPEq0n4Mqa/HGKIhSkIHeL5AyhkYV8i59U5AR6csBvApHHNl/vI1Bx" crossorigin="anonymous">
+  </head>
+  <body>
+    <h1>Courses</h1>
+<table class="table table-striped">
+  <thead>
+    <tr>
+      <th>ID</th>
+      <th>Prefix</th>
+      <th>Number</th>
+      <th>Description</th>
+      <th></th>
+    </tr>
+  </thead>
+  <tbody>
+    <?php
 $servername = "localhost";
 $username = "asoltiso_asoltis";
 $password = "OUcreate!";
@@ -13,39 +30,36 @@ $conn = new mysqli($servername, $username, $password, $dbname);
 // Check connection
 if ($conn->connect_error) {
   die("Connection failed: " . $conn->connect_error);
-} 
+}
 
-$sql = "SELECT * FROM Cars";
+$sql = "SELECT * from car";
 $result = $conn->query($sql);
-       
- if ($result->num_rows > 0) {
-    // output data of each row
-    while($row = $result->fetch_assoc()) {
 
-
-        echo '<tr>
-                  <td scope="row">' . $row["CarID"]. '</td>
-                  <td>' . $row["Color"] .'</td>
-                  <td> '.$row["Make"] .'</td>
-                  <td> '.$row["Year"] .'</td>
-                  <td>
-      <form method="post" action="course-section.php">
-        <input type="hidden" name="id" value="<?=$row["course_id"]?>" />
+if ($result->num_rows > 0) {
+  // output data of each row
+  while($row = $result->fetch_assoc()) {
+?>
+  <tr>
+    <td><?=$row["CarID"]?></td>
+    <td><?=$row["Color"]?></td>
+    <td><?=$row["Make"]?></td>
+    <td><?=$row["Year"]?></td>
+    <td>
+      <form method="post" action="page5post.php">
+        <input type="hidden" name="id" value="<?=$row["Color"]?>" />
         <input type="submit" value="Sections" />
       </form>
     </td>
-                  </tr>';
-    }
+  </tr>
+<?php
+  }
 } else {
-    echo "0 results";
-} 
+  echo "0 results";
+}
+$conn->close();
 ?>
-       </tbody>
-    </div>
-</table>
-<body>
-    <form action="page5post.php" method="post">
-Sort by Color: <input type="text" name="color"><br>
-<input type="submit">
-</form>
+  </tbody>
+    </table>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-A3rJD856KowSb7dwlZdYEkO39Gagi7vIsF0jrRAoQmDKKtQBHUuLZ9AsSv4jD4Xa" crossorigin="anonymous"></script>
   </body>
+</html>
