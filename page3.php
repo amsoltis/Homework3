d0cba8cee4bd4bcb23463d475ba0eaf2c8991961
Andrew Soltis <ams.soltis@ou.edu>
2022-09-21 14:01:36 -0500

    test

--- page3.php
+++ page3.php
@@ -1,8 +1,26 @@
-<?php require_once("header.php"); ?>
 <!doctype html>
 <html lang="en">
-
-<?php
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Sections</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-gH2yIJqKdNHPEq0n4Mqa/HGKIhSkIHeL5AyhkYV8i59U5AR6csBvApHHNl/vI1Bx" crossorigin="anonymous">
+  </head>
+  <body>
+    <h1>Sections</h1>
+<table class="table table-striped">
+  <thead>
+    <tr>
+      <th>InstructorID</th>
+      <th>LastName</th>
+      <th>FirstName</th>
+      <th>CourseID</th>
+      <th>CourseNumber</th>
+      <th>Section</th>
+    </tr>
+  </thead>
+  <tbody>
+    <?php
 $servername = "localhost";
 $username = "asoltiso_asoltis";
 $password = "OUcreate!";
@@ -13,33 +31,35 @@ $conn = new mysqli($servername, $username, $password, $dbname);
 // Check connection
 if ($conn->connect_error) {
   die("Connection failed: " . $conn->connect_error);
-} 
+}
 $iid = $_GET['id'];
-$sql = "SELECT I.InstructorID, LastName, FirstName, CourseID, CourseNumber, Section FROM Instructor I Join Courses C on I.InstructorID = C.InstructorID where I.InstructorID=".$iid;
-$result = $conn->query($sql);
-       
- if ($result->num_rows > 0) {
-    // output data of each row
+//echo $iid;
 
-    while($row = $result->fetch_assoc()) {
+$sql = "Select I.InstructorID, LastName, FirstName, CourseID, CourseNumber, Section FROM Instructor I Join Courses C on I.InstructorID = C.InstructorID where I.InstructorID=" . $iid;
+//echo $sql;
+    $result = $conn->query($sql);
 
-   
-        echo '<tr>
-                  <td scope="row">' . $row["LastName"]. '</td>
-                  <td> '.$row["FirstName"] .'</td>
-                  <td> '.$row["CourseID"] ..'</td>
-                  <td>' . $row["CourseNumber"] .'</td>
-                  <td>' . $row["I.InstructorID"] .'</td>
-                  <br>
-                  </tr>';
-    }
-} else {
-    echo "0 results";
-} 
+if ($result->num_rows > 0) {
+  // output data of each row
+  while($row = $result->fetch_assoc()) {
 ?>
-       </tbody>
-    </div>
-</table>
-
-<p><a href="./page1.php">Back to Instructor Tables</a></p>
-<p><a href="./page2.php">Back to Courses Tables</a></p>
+  <tr>
+    <td><?=$row["InstructorID"]?></td>
+    <td><?=$row["LastName"]?></td>
+    <td><?=$row["FirstName"]?></td>
+    <td><?=$row["CourseID"]?></td>
+    <td><?=$row["CourseNumber"]?></td>
+    <td><?=$row["Section"]?></td>
+  </tr>
+<?php
+  }
+} else {
+  echo "0 results";
+}
+$conn->close();
+?>
+  </tbody>
+    </table>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-A3rJD856KowSb7dwlZdYEkO39Gagi7vIsF0jrRAoQmDKKtQBHUuLZ9AsSv4jD4Xa" crossorigin="anonymous"></script>
+  </body>
+</html>
