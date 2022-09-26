3775b04972f109b9c732cf3933898100133d4b1d
amsoltis <97993404+amsoltis@users.noreply.github.com>
2022-09-26 14:07:38 -0500

    Update page5.php

--- page5.php
+++ page5.php
@@ -1,11 +1,5 @@
-<!doctype html>
-<?php require_once("header.php"); ?>
-<html lang="en">
   <head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>Cars</title>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-gH2yIJqKdNHPEq0n4Mqa/HGKIhSkIHeL5AyhkYV8i59U5AR6csBvApHHNl/vI1Bx" crossorigin="anonymous">
+    <?php require_once("header.php"); ?>
   </head>
   <body>
     <h1>Cars</h1>
@@ -63,8 +57,7 @@ $conn->close();
     </table>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-A3rJD856KowSb7dwlZdYEkO39Gagi7vIsF0jrRAoQmDKKtQBHUuLZ9AsSv4jD4Xa" crossorigin="anonymous"></script>
   </body>
-</html>
 
-<body>
+<footer>
 Click the 'Same Color' button to filter by cars of the same color. 
-</body>
+</footer>
