0034bfed67fdc8d90c3b3ddfaf37d1e13728bc7b
amsoltis <97993404+amsoltis@users.noreply.github.com>
2022-09-26 14:04:51 -0500

    Update page2.php

--- page2.php
+++ page2.php
@@ -1,12 +1,7 @@
-<!doctype html>
-<?php require_once("header.php"); ?>
-<html lang="en">
   <head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>Courses</title>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-gH2yIJqKdNHPEq0n4Mqa/HGKIhSkIHeL5AyhkYV8i59U5AR6csBvApHHNl/vI1Bx" crossorigin="anonymous">
+    <?php require_once("header.php"); ?>
   </head>
+<html>
   <body>
     <h1>Courses</h1>
 <table class="table table-striped">
@@ -56,8 +51,7 @@ $conn->close();
     </table>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-A3rJD856KowSb7dwlZdYEkO39Gagi7vIsF0jrRAoQmDKKtQBHUuLZ9AsSv4jD4Xa" crossorigin="anonymous"></script>
   </body>
-</html>
-
-<body>
+  
+<footer>
 Click the 'InstructorID' to see Instructor Name and other courses taught by that instructor
-</body>
+</footer>
