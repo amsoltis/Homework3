6e17d30ea4ced328f3c545c097933d393aa34f73
amsoltis <97993404+amsoltis@users.noreply.github.com>
2022-09-26 14:05:30 -0500

    Update page3.php

--- page3.php
+++ page3.php
@@ -1,12 +1,7 @@
-<!doctype html>
-<?php require_once("header.php"); ?>
-<html lang="en">
   <head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>Sections</title>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-gH2yIJqKdNHPEq0n4Mqa/HGKIhSkIHeL5AyhkYV8i59U5AR6csBvApHHNl/vI1Bx" crossorigin="anonymous">
+    <?php require_once("header.php"); ?>
   </head>
+  
   <body>
     <h1>Sections</h1>
 <table class="table table-striped">
@@ -56,4 +51,3 @@ $conn->close();
     </table>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-A3rJD856KowSb7dwlZdYEkO39Gagi7vIsF0jrRAoQmDKKtQBHUuLZ9AsSv4jD4Xa" crossorigin="anonymous"></script>
   </body>
-</html>
