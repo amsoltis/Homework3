d69210d442ec909dd779e7c053ce99a4849cf2e7
amsoltis <97993404+amsoltis@users.noreply.github.com>
2022-09-26 14:03:33 -0500

    Update page1.php

--- page1.php
+++ page1.php
@@ -1,13 +1,8 @@
-<!doctype html>
-<?php require_once("header.php"); ?>
-<html lang="en">
   <head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>Instructors</title>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-gH2yIJqKdNHPEq0n4Mqa/HGKIhSkIHeL5AyhkYV8i59U5AR6csBvApHHNl/vI1Bx" crossorigin="anonymous">
-  </head>
-  <body>
+<?php require_once("header.php"); ?>
+</head>
+
+<body>
     <h1>Instructors</h1>
 <table class="table table-striped">
   <thead>
@@ -54,7 +49,7 @@ $conn->close();
     </table>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-A3rJD856KowSb7dwlZdYEkO39Gagi7vIsF0jrRAoQmDKKtQBHUuLZ9AsSv4jD4Xa" crossorigin="anonymous"></script>
   </body>
-</html>
-<body>
+
+<footer>
 Click the 'Name' to see only that Instructors Information
-</body>
+</footer>
