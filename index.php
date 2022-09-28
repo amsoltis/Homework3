a6a4998dea45fb73e06f320864a6ee48041929c1
amsoltis <97993404+amsoltis@users.noreply.github.com>
2022-09-28 13:22:35 -0500

    Update index.php

--- index.php
+++ index.php
@@ -6,7 +6,7 @@
   <body>
       <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-A3rJD856KowSb7dwlZdYEkO39Gagi7vIsF0jrRAoQmDKKtQBHUuLZ9AsSv4jD4Xa" crossorigin="anonymous"></script>
       <br>Instructor - Table that shows instructors, and allows you to filter by 'Name'. Filters to only one result as its a appears once in Table.</br>
-      <br>Courses - Table that shows courses taught, and allows you to filter by 'Instructor ID' to see only courses taught by that professor.</br>
+      <br>Courses - Table that shows courses taught. Can click instructor ID to see all courses taught by all instructors.</br>
       <br>Birds - Table shows information about birds, no filter.</br>
       <br>Cars - Table shows information about cars, allows you to filter by car color.</br>
   </body>
